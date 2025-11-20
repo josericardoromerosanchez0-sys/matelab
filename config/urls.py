ed64e4448c25a3d7d4976ea0c7d94070faf5297e
Jose <josericardoromerosanchez0@gmail.com>
2025-11-20 17:03:12 -0500

    cambio

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from web_project.views import SystemView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),

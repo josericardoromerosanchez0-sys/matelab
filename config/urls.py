01476078defdcbee161dda672b3ed321c8611aee
Jose <josericardoromerosanchez0@gmail.com>
2025-11-20 15:12:26 -0500

    quitar redirección a pagina de error

--- config/urls.py
+++ config/urls.py
@@ -37,7 +37,3 @@ urlpatterns = [
     # Biblioteca urls
     path('biblioteca/', include('apps.biblioteca.urls', namespace='biblioteca')),
 ]
-
-handler404 = SystemView.as_view(template_name="pages_misc_error.html", status=404)
-handler400 = SystemView.as_view(template_name="pages_misc_error.html", status=400)
-handler500 = SystemView.as_view(template_name="pages_misc_error.html", status=500)

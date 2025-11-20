68679aaf1a65ec554c99b162e28c6179b096e31f
Jose <josericardoromerosanchez0@gmail.com>
2025-11-20 16:27:59 -0500

    cambio

--- config/settings.py
+++ config/settings.py
@@ -185,13 +185,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "src" / "assets"/ "static",
+    BASE_DIR / "src" / "assets",
+    BASE_DIR / "src" / "assets" / "static",
+    BASE_DIR / "static",
 ]
 
 # Default URL on which Django application runs for specific environment
